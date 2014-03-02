fe4e51751e116df8a152dd76fba8b4609cb49823
Vasili Kaloidis <vas.kaloidis@gmail.com>
2014-03-02 17:58:08 -0500

    Added Ticket Create Page
    Started the Ticket Create Page

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 include './head.php';
 
-if(isset($_COOKIE["authenticated"])){
-
-	include './home_page.php';
+//if(isset($_COOKIE["authenticated"])){
+if(TRUE)
+	//include './home_page.php';
+	include './ticket_create.php';
+	
+	
 	
 } else {
 	//include 'front_page.php';
