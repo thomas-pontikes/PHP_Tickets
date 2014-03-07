3b64bdff25d7b00c8092f657508f9e6407704f98
thomas-pontikes <tapontikes@gmail.com>
2014-03-07 16:25:48 -0500

    Ticket Formatting

--- ticket_create_action.php
+++ ticket_create_action.php
@@ -1,15 +1,15 @@
 
 <?php
   
-echo $_GET['ticket_holder_name'];
-echo $_GET['ticket_date'] ;
-echo $_GET['ticket_time'];
-echo $_GET['ticket_origin'] ;
-echo $_GET['ticket_destination'];
-echo $_GET['trip_origin'] ;
-echo $_GET['trip_destination'] ;
-echo $_GET['ticket_price'];
-echo $_GET['conformation'];	
+$_GET['ticket_holder_name'];
+$_GET['ticket_date'] ;
+$_GET['ticket_time'];
+$_GET['ticket_origin'] ;
+$_GET['ticket_destination'];
+$_GET['trip_origin'] ;
+$_GET['trip_destination'] ;
+$_GET['ticket_price'];
+$_GET['conformation'];	
 
 include './ticket.php';
 		
