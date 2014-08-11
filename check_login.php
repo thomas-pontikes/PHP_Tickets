41a9fa3e8f37c66d73b32f55281d5b9954a94fb3
thomas-pontikes <thomas.pontikes@uconn.edu>
2014-08-11 16:03:41 -0400

    Added md5 encryption

--- check_login.php
+++ check_login.php
@@ -16,7 +16,8 @@ mysql_connect($host, $user, $pass);
 mysql_select_db($db);
 if (isset($_POST['username'])) {
 	$username = $_POST['username'];
-	$password = $_POST['password'];
+	$encryptme = $_POST['password'];
+	$password = md5($encryptme);
 $sql = "SELECT * FROM users WHERE username='".$username."' AND password='".$password."' LIMIT 1";
 $res = mysql_query($sql);
 if (mysql_num_rows($res) == 1) {
