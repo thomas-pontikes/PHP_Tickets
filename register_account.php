8ef452c298244ef0ff770556cd8f7cff8802039e
root <thomas.pontikes@uconn.edu>
2014-08-14 10:27:06 -0400

    registration page

--- register_account.php
+++ register_account.php
@@ -27,9 +27,7 @@ h2 {
 	position: absolute;
 	text-align:center;
 	width: 100%;
-	top: 70%;
-	
-	
+	top: 70%;	
 }
 
 </style>
@@ -37,21 +35,44 @@ h2 {
 <div id="wrapper">
 <h1>Register:</h1>
 <br><br>
-<form id="form" action="register.php" method="post">
+<form id="form" action="register_account.php" method="post">
 User Name:
 <br>
-<input type="text" name="username" ></input>
+<input type="text" name="username" placeholder="username" ></input>
 <br><br>
 Password:
 <br>
-<input type="password" name="password" ></input>
+<input type="password" name="password" placeholder="password" ></input>
 <br><br>
 Re-type Password:
 <br>
-<input type="password" name="repassword"></input>
+<input type="password" name="repassword"placeholder="repeat password" ></input>
 <br>
 <input type="submit" name="submit" value="Register"></input>
 </form>
 </div>
 </body>
 </html>
+<?php
+
+if(isset($_POST['submit'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$repassword = $_POST['repassword'];
+if($username && $password && repassword){
+	if($password==$repassword){
+		$connect = mysql_connect("127.0.0.1","root","tap13002");
+		mysql_select_db("users");
+		$query = mysql_query(INSERT INTO users VALUES('','$useranme','$password');
+		echo "You have been registered, please login";
+	}
+	else{
+	echo "Passwords do not match, please try again";
+	}
+}
+else{
+echo"all fields must be filled in";
+}
+
+}
+?>
