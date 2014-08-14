0cfd8b5d3e250363715ca4a431d108bce28d85e5
thomas-pontikes <thomas.pontikes@uconn.edu>
2014-08-14 12:06:49 -0400

    Registration page

--- register.php
+++ register.php
@@ -1,23 +1,55 @@
-<?php
-$username = strip_tags $_POST['username'];
-$password = strip_tags $_POST['password']);
-$repassword = strip_tags $_POST['repassword']);
-
-if ($username&&$password%%$repassword){
-	
-	$password = hash('sha256', $_POST['password']);
-	$repassword = hash('sha256', $_POST['repassword']);
-	
-
-$connect = mysql_connect("localhost","root","tap13002")
-	mysql_select_db("secure_login");
-
-$queryreg = mysql_query ("
-	
-	INSERT INTO users VALUES ('','$username','$password')
-	
-	")
-	die("Return to index.php")
-		
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<title>PHP_Tickets Register Page</title>
+<meta charset="UTF-8">
+<style type="text/css">
+html {
+	font-family: Verdana, Geneva, sans-serif;
 }
-?>
+h1 {
+	font-size: 24px;
+	text-align: center;
+
+}
+h2 {
+	font-size: 12px;
+	text-align: center;
+}
+#wrapper {
+	position: absolute;
+	width: 100%;
+	top: 30%;
+	margin-top: -50px;/* half of #content height*/
+}
+#form {
+	position: absolute;
+	text-align:center;
+	width: 100%;
+	top: 70%;	
+}
+
+</style>
+<body>
+<div id="wrapper">
+<h1>Register:</h1>
+<br><br>
+<form id="form" action="register_account.php" method="post">
+User Name:
+<br>
+<input type="text" name="username" placeholder="username" ></input>
+<br><br>
+Password:
+<br>
+<input type="password" name="password" placeholder="password" ></input>
+<br><br>
+Re-type Password:
+<br>
+<input type="password" name="repassword"placeholder="repeat password" ></input>
+<br>
+<input type="submit" name="submit" value="Register"></input>
+</form>
+</div>
+</body>
+</html>
