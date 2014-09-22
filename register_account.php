50d049c371ff273dfd358d34df9048e027a62fa3
Thomas Pontikes <thomas.pontikes@uconn.edu>
2014-09-22 19:53:13 -0400

    format

--- register_account.php
+++ register_account.php
@@ -2,6 +2,8 @@
 
 include('register.php');
 
+$con = mysqli_connect('localhost','root','tap13002','secure_login');		
+
 if ( isset($_SERVER['HTTP_CLIENT_IP']) && ! empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
 } elseif ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && ! empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -21,23 +23,20 @@ $ip = ($ip === false) ? '0.0.0.0' : $ip;
 if(isset($_POST['submit'])){
 	
 if($username&&$password&&$repassword&&$email){
-		$encpassword = hash('sha256', $_POST['password']);
-		$encrepassword = hash('sha256', $_POST['repassword']);
-	}
-else {
+$encpassword = hash('sha256', $_POST['password']);
+$encrepassword = hash('sha256', $_POST['repassword']);
+}else{
 echo '<html><div class="pos_fixed"<h1>Attention:<br>Please enter text in all fields</h1></div></html>';
 exit();
 }
 
-if($encpassword==$encrepassword){
+if($encpassword==$encrepassword){	
+$sql = mysql_query("SELECT * FROM users (username, password, email, ip_address) WHERE username=$username");
 	
-	$sql = mysql_query("SELECT * FROM users WHERE username=$username");
-	
-	if(mysql_num_rows($sql) > 1){
+if(mysql_num_rows($sql) >= 1){
 		echo '<html><div class="pos_fixed" <h1>Attention:<br><br>That user already exists!</h1></div></html>';
 			exit();
-	}
-	$con = mysqli_connect('localhost','root','tap13002','secure_login');		
+		}else{
 	mysqli_query($con,"INSERT INTO users (username, password, email, ip_address) VALUES ('$username','$encpassword','$email','$ip')");
 	echo '<html><div class="pos_fixed" <h1>Success <br>You are now registered, please login <a href="index.php"> here </a></h1></div></html>';
 }
@@ -45,6 +44,7 @@ if($encpassword==$encrepassword){
 else{
 	echo '<html><div class="pos_fixed" <h1>Attention:<br>Passwords do not match!</h1></div></html>';
 	exit();
+}
 }
 }
 
