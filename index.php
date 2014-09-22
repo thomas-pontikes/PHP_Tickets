9510d5ed5a6cfd0853e43f77656ae354606d49f6
Thomas Pontikes <thomas.pontikes@uconn.edu>
2014-09-22 15:28:37 -0400

    Changed login

--- index.php
+++ index.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Basic login system</title>
-<style type="text/css">
-html {
-	font-family: Verdana, Geneva, sans-serif;
-}
-h1 {
-	font-size: 24px;
-	text-align: center;
-}
-h2 {
-	font-size: 12px;
-	text-align: center;
-}
-#wrapper {
-	position: absolute;
-	width: 100%;
-	top: 30%;
-	margin-top: -50px;/* half of #content height*/
-}
-#form {
-	margin: auto;
-	width: 200px;
-	height: 100px;
-}
-</style>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
 
-</head>
- 
-<body>
-<div id="wrapper">
-<h1>To Continue Please Login</h1>
-<form id="form" action="check_login.php" method="post" enctype="multipart/form-data">
-<h2>If you need to register an account please <a href="register_account.php"> click here </a>
-	<br>
-	<br>
-Username: <input type="text" name="username" /> <br />
-Password: <input type="password" name="password" /> <br />
-<input type="submit" value="Login" name="Submit" />
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+
+<div class="container" style="margin-top:30px">
+<div class="col-md-4">
+ <div class="panel panel-default">
+  <div class="panel-heading"><h3 class="panel-title"><strong>Sign In </strong></h3></div>
+  <div class="panel-body">
+	  <form method="post" action="check_login.php" class="form-signin" role="form">
+	          <h2 class="form-signin-heading">Please sign in</h2>
+	          <input type="email" class="form-control" placeholder="Email address" name="username" autofocus="">
+			  <br>
+	          <input type="password" class="form-control" placeholder="Password" name="password" >
+			  <br>
+	          <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			  <br>
+	        </form>
+			
+   
 </form>
-</body>
-</html>
+</div>
+</div>
+</div>
+<hr>
+
 
 
